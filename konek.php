0593134c58a036ba60bfb32e820487441d7431d1
wilsonyuann <wilsonyuann@yahoo.com>
2016-11-06 14:20:32 +0700

    e

--- konek.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $hostname  = "localhost";
-   $username  = "root";
-   $password  = "";
-   $dbname  = "percobaan";
-   $db = mysql_connect($hostname, $username, $password) or die ('Koneksi Gagal! ');
-   mysql_select_db($dbname);
-?>
